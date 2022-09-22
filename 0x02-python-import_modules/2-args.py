919d2b70aa2c51030a6cef235135ad5053132847
Domengo <dominicsengo@gmail.com>
2022-09-22 20:58:16 +0000

    easy print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
 
-    n = len(argv)
-
-    if (n == 0):
-        print("{} arguments.".format(n))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        if (n == 2):
-            print("{} argument:".format(n - 1))
-        else:
-            print("{} arguments:".format(n - 1))
-        for j in range(1, n):
-            print("{}: {}".format(j, argv[j]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))

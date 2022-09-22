0181bc4343b26a30fd691c726f38b1cbe5bbb2d9
Domengo <dominicsengo@gmail.com>
2022-09-22 13:43:51 +0000

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+n = len(argv)
+if(n < 2):
+    print("{} arguement".format(n - 1))
+else:
+    if(n == 2):
+        print("{} arguement:".format(n - 1))
+    else:
+        print("{} arguements:".format(n - 1))
+    for n in range(1, n):
+        print("{}: {}".format(n, argv[n]))

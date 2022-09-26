381add2e85b65d0e4151344b2e1803584583de7e
Domengo <dominicsengo@gmail.com>
2022-09-26 10:52:04 +0000

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
+
     l = len(my_string)
 
     g = 0
@@ -10,6 +12,7 @@ def no_c(my_string):
         if (my_string[i] == 'c' or my_string[i] == 'C'):
             new_string = new_string[:(i - g)] + my_string[(i + 1):]
             g += 1
+
     return (new_string)
 
 """

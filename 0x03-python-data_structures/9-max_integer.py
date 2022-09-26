21cd17a17422dcbdc6e8ce6027605d119c8c4eb5
Domengo <dominicsengo@gmail.com>
2022-09-26 12:02:08 +0000

    big

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    else:
+        big = my_list[0]
+        for i in my_list:
+            if i > big:
+                big = i
+        return big

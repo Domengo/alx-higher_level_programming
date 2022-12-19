c3a81e39f44f189d653f3c20aa09287ec4bbb65b
Domengo <dominicsengo@gmail.com>
2022-12-20 00:39:20 +0300

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-const conj = 'is';
-argv.forEach((argv, conj) => {
-  console.log(argv[3] + conj + argv[4]);
-});
+const conj = ' is ';
+console.log(argv[2] + conj + argv[3]);

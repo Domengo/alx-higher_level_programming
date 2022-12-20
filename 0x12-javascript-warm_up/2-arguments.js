2bf62ee4e61f1553103113a99977c5b9e5fed339
Domengo <dominicsengo@gmail.com>
2022-12-20 15:15:25 +0300

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,7 +7,7 @@ const args = process.argv;
 if (/* args.length > 2 && */args.length === 3) {
   console.log('Argument found');
 } else if (args.length > 3) {
-  console.log('Argument found');
+  console.log('Arguments found');
 } else /* if (args.length = 2) */{
   console.log('No argument');
 }

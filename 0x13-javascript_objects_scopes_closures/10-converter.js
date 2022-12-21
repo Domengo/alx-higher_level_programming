0b07f59f27ee4df2e11c82be945198ecd03ecda9
Domengo <dominicsengo@gmail.com>
2022-12-22 02:35:09 +0300

    more js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function (num) {
-    let result = '';
-
-    while (num > 0) {
-      result = (num % base) + result;
-      num = Math.floor(num / base);
-    }
-
-    return result;
-  };
+  function conv (n) {
+    return n.toString(base);
+  }
+  return n;
 };

d31b2c547f3061dd43c0d3787aeea3ab3acc1897
Domengo <dominicsengo@gmail.com>
2022-12-24 14:06:18 +0300

    orm

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,7 +3,8 @@
 Lists all states from the database hbtn_0e_0_usa.
 Usage: ./0-select_states.py <mysql username> \
                             <mysql password> \
-                             <database name>
+                             <database name> \
+                             <state name searched>
 """
 
 import MySQLdb
@@ -13,7 +14,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   (sys.argv[4],))
     results = cursor.fetchall()
     for row in results:
         print(row)

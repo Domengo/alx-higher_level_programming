dc0b91fe28815da39586a50049026ea6cbb9e860
Domengo <dominicsengo@gmail.com>
2022-12-24 03:24:09 +0300

    orm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="Dicklongflop?7", db=sys.argv[3])
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+cursor.close()
+db.close()

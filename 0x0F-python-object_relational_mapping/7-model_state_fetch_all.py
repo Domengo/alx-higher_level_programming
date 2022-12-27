db242a8f099e5065a6ac2a1bad7aa748c4ee0812
Domengo <dominicsengo@gmail.com>
2022-12-28 02:31:47 +0300

    orm

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+lists all states from db hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]))
+    session = Session(bind=engine)
+    states = session.query(State).order_by(State.id).all()
+
+    for row in states:
+        print(f"{row.id}: {row.name}")

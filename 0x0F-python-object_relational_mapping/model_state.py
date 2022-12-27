36ba22d3b3afe3688da1c551cda202dc9025207e
Domengo <dominicsengo@gmail.com>
2022-12-28 01:51:07 +0300

    orm

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+"""create state class that inherits from base"""
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)

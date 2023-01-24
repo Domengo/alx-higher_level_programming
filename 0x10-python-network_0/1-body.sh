099a44113c2e592778225e79b9adc9932396f973
Domengo <dominicsengo@gmail.com>
2023-01-25 01:17:49 +0300

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
 # check if a URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Please provide a URL as an argument"
-  exit 1
-fi
-
-# send a GET request to the URL and store the response in a variable
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-# check if the response status code is 200
-if [ "$response" -eq 200 ]; then
-  # display the body of the response
-  curl -s "$1"
-else
-  echo "Error: Response status code is not 200"
-fi
+curl -sL "$1"

59be5c0eeac78bebca36f495ebc62bd4b06719ce
Domengo <dominicsengo@gmail.com>
2023-01-27 01:44:37 +0300

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,10 @@ and displays the body of the response (decoded in utf-8)
 import urllib.parse as parse
 import urllib.request as request
 from sys import argv
-values = {'email': argv[2]}
-data = parse.urlencode(values).encode('utf-8')
-req = request.Request(argv[1], data)
-with request.urlopen(req) as r:
-    print(r.read().decode('utf-8'))
+
+if __name__ == '__main__':
+    values = {'email': argv[2]}
+    data = parse.urlencode(values).encode('utf-8')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as r:
+        print(r.read().decode('utf-8'))

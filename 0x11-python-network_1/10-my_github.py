165c4acec4e745a9be693f7da06935dd28c021da
Domengo <dominicsengo@gmail.com>
2023-01-27 00:31:13 +0300

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,10 +7,11 @@ and uses the GitHub API to display your id
 import requests
 from sys import argv
 
-url = 'https://api.github.com/user'
-username = argv[1]
-pat = argv[2]
-
-r = requests.get(url, auth=(username, pat))
-data = r.json().get("id")
-print(data)
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    username = argv[1]
+    pat = argv[2]
+    
+    r = requests.get(url, auth=(username, pat))
+    data = r.json().get("id")
+    print(data)

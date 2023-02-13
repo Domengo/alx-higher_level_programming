c537a1ef62cc2c3f26f402c3f88d7fe7dba470d8
Domengo <dominicsengo@gmail.com>
2023-02-13 18:11:50 +0300

    completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-const { json } = require('stream/consumers');
-const url = 'https://jsonplaceholder.typicode.com/todos';
+const url = process.argv[2];
 
 request(url, (err, response, body) => {
-    if (err) {
-        console.log(err);
+  if (err) {
+    console.log(err);
+  }
+  const completed = {};
+  const tasks = JSON.parse(body);
+  for (const i in tasks) {
+    const task = tasks[i];
+    if (task.completed === true) {
+      if (completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      }
     }
-    const completed = {};
-    const json = JSON.parse(body);
-    for (const i in json) {
-        const task = json[i];
-        if (task.completed == true) {
-            if (completed[task.userId] === undefined) {
-                completed[task.userId] = 1;
-            }
-        }
-    
-    }
-    console.log(completed);
+  }
+  console.log(completed);
 });

c8f998902d642807c8fb89d4a37f7b21fe6bb299
Domengo <dominicsengo@gmail.com>
2023-02-13 16:08:44 +0300

    fix semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,8 @@ const request = require('request');
 const URL = process.argv[2];
 
 request(URL, (err, response) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log('code: ' + response.statusCode);
+  if (err) {
+    console.log(err);
+  }
+  console.log('code: ' + response.statusCode);
 });
